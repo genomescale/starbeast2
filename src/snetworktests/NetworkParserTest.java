d3270d5e23b75c8ff6fa23ad57dd123ebbc22bac
Chi Zhang <zhangchicool@gmail.com>
2016-03-01 23:11:11 +0100

    updated network parser test

--- src/snetworktests/NetworkParserTest.java
+++ src/snetworktests/NetworkParserTest.java
@@ -10,12 +10,12 @@ import speciesnetwork.NetworkParser;
 public class NetworkParserTest {
     TreeParser treeParser;
     NetworkParser networkParser;
-    
-    // final String testNetwork = "((1:1.4,((2:0.7,(3:0.2,(4:0.1)Y#H1:0.1)g:0.5)e:0.3,(((Y#H1:0.2,5:0.3)h:0.5,6:0.8)f:0.1)X#H2:0.1)c:0.4)a:0.5,((X#H2:0.1,7:1.0)d:0.6,8:1.6)b:0.3)r";
 
+    // "Partial" reticulation nodes (that is, #H nodes without any directly attached children in the extended newick string)
+    // should be on the right. "Complete" reticulation nodes (with directly attached children) should be on the left.
     // cannot parse if there is :: inheritProb in the string
     final String testNetwork2 = "((A:0.2,#H1:0.1)#S1:0.3,((B:0.1)#H1:0.2,C:0.3)#S2:0.2)#R";
-    // final String testNetwork2 = "((A:0.2,(B:0.1)#H1:0.1)#S1:0.3,(#H1:0.2,C:0.3)#S2:0.2)#R";
+    // final String testNetwork2 = "(((B:0.1)#H1:0.1,A:0.2)#S1:0.3,(C:0.3,#H1:0.2)#S2:0.2)#R";
 
     final String testNetwork3 = "((((A:0.1)#H1:0.1)#H2:0.3,#H4:0.1)#S2:0.1,((((#H1:0.1)#H3:0.1,#H2:0.1)#S1:0.1)#H4:0.1,#H3:0.3)#S3:0.1)#R";
 
--- src/speciesnetwork/RebuildEmbedding.java
+++ src/speciesnetwork/RebuildEmbedding.java
@@ -88,7 +88,6 @@ public class RebuildEmbedding extends Operator {
         try {
             reinitializeEmbedding();
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
