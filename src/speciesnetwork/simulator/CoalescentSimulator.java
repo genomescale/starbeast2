29f77e63f86f87e96880d163d026dc35f011b2bd
Chi Zhang <zhangchicool@gmail.com>
2016-04-11 16:17:26 +0200

    changed the simulator to extend runnable instead of operator

--- src/speciesnetwork/simulator/CoalescentSimulator.java
+++ src/speciesnetwork/simulator/CoalescentSimulator.java
@@ -1,12 +1,14 @@
 package speciesnetwork.simulator;
 
+import java.io.IOException;
 import java.util.*;
 
 import beast.app.seqgen.*;
 import beast.core.Description;
 import beast.core.Input;
 import beast.core.Input.Validate;
-import beast.core.Operator;
+import beast.core.Runnable;
+import beast.core.State;
 import beast.core.parameter.IntegerParameter;
 import beast.core.parameter.RealParameter;
 import beast.evolution.alignment.Alignment;
@@ -26,106 +28,134 @@ import com.google.common.collect.Multimap;
  */
 
 @Description("Simulate gene trees given a species network (multispecies coalescent).")
-public class CoalescentSimulator extends Operator {
+public class CoalescentSimulator extends Runnable {
     public Input<Network> speciesNetworkInput =
             new Input<>("speciesNetwork", "Species network for embedding the gene tree.", Validate.REQUIRED);
-    public Input<Tree> geneTreeInput =
-            new Input<>("geneTree", "Gene tree embedded in the species network.", Validate.REQUIRED);
-    public Input<IntegerParameter> embeddingInput =
-            new Input<>("embedding", "Map of gene tree traversal within the species network.", Validate.REQUIRED);
     public Input<RealParameter> gammaInput =
             new Input<>("gamma", "Inheritance probabilities (traversing left backward in time).", Validate.REQUIRED);
     public Input<RealParameter> popSizesInput =
             new Input<>("popSizes", "Constant per-branch population sizes.", Validate.REQUIRED);
-    public Input<Double> ploidyInput =
-            new Input<>("ploidy", "Ploidy (copy number) for this gene (default is 2).", 2.0);
     public Input<TaxonSet> taxonSuperSetInput =
             new Input<>("taxonSuperset", "Super-set of taxon sets mapping lineages to species.", Validate.REQUIRED);
-    public Input<SequenceSimulator> seqSimulatorInput = new Input<>("sequenceSimulator", "Sequence simulator.");
+
+    public Input<List<Tree>> geneTreesInput =
+            new Input<>("geneTree", "Gene tree embedded in the species network.", new ArrayList<>());
+    public Input<List<IntegerParameter>> embeddingsInput =
+            new Input<>("embedding", "Map of gene tree traversal within the species network.", new ArrayList<>());
+    public Input<RealParameter> ploidiesInput =
+            new Input<>("ploidy", "Ploidy (copy number) for each gene (default is 2).");
+    public Input<State> startStateInput =
+            new Input<>("state", "elements of the state space", Validate.REQUIRED);
+
+    public Input<List<SequenceSimulator>> seqSimulatorsInput =
+            new Input<>("sequenceSimulator", "Sequence simulator.", new ArrayList<>());
 
     private Network speciesNetwork;
-    private Tree geneTree;
-    private IntegerParameter embedding;
     private RealParameter gammaP;
     private RealParameter popSizes;
-    private double ploidy;
+    private List<Tree> geneTrees;
+    private List<IntegerParameter> embeddings;
+    private RealParameter ploidies;
 
     private Multimap<NetworkNode, Node> networkNodeGeneLineagesMap = HashMultimap.create();
-    private Multimap<NetworkNode, Node> tipsMap = HashMultimap.create();
-
     private int nodeIndex;  //node index number
     private int speciesLeafNodeCount;
+
+    private List<SequenceSimulator> seqSimulators;
 
     @Override
     public void initAndValidate() {
         speciesNetwork = speciesNetworkInput.get();
-        geneTree = geneTreeInput.get();
-        embedding = embeddingInput.get();
         gammaP = gammaInput.get();
-        ploidy = ploidyInput.get();
         popSizes = popSizesInput.get();
-        // assert popSizes.getDimension() == speciesNetwork.getBranchCount();
+        geneTrees = geneTreesInput.get();
+        embeddings = embeddingsInput.get();
+        ploidies = ploidiesInput.get();
+        seqSimulators = seqSimulatorsInput.get();
 
-        // generate map of tip names to tip nodes
-        final Map<String, NetworkNode> speciesNodeMap = new HashMap<>();
-        for (NetworkNode leafNode: speciesNetwork.getLeafNodes()) {
-            final String speciesName = leafNode.getID();
-            speciesNodeMap.put(speciesName, leafNode);
-        }
-        final Map<String, Node> geneNodeMap = new HashMap<>();
-        for (Node leafNode: geneTree.getExternalNodes()) {
-            final String geneName = leafNode.getID();
-            geneNodeMap.put(geneName, leafNode);
-        }
-        // generate multimap of species network tip node to gene tree tip nodes
-        final TaxonSet taxonSuperSet = taxonSuperSetInput.get();
-        for (Taxon speciesTip: taxonSuperSet.taxonsetInput.get()) {
-            final NetworkNode speciesNode = speciesNodeMap.get(speciesTip.getID());
-            final TaxonSet speciesTaxonSet = (TaxonSet) speciesTip;
-            for (Taxon geneTip: speciesTaxonSet.taxonsetInput.get()) {
-                final Node geneNode = geneNodeMap.get(geneTip.getID());
-                tipsMap.put(speciesNode, geneNode);
-            }
-        }
+        // sanity check
+        if (geneTrees == null || embeddings == null || geneTrees.size() != embeddings.size())
+            throw new RuntimeException("Check gene tree and embedding input!");
+
+        // initialize state nodes, essential
+        State state = startStateInput.get();
+        state.initialise();
     }
 
     @Override
-    public double proposal() {
-        // initialize embedding matrix to -1 (no traversal)
+    public void run() throws IOException {
+        // check correctness of parameter dimensions
+        final int speciesBranchCount = speciesNetwork.getBranchCount();
+        final int speciesReticulationNodeCount = speciesNetwork.getReticulationNodeCount();
+        if (popSizes.getDimension() != speciesBranchCount)
+            popSizes.setDimension(speciesBranchCount);
+        if (gammaP.getDimension() != speciesReticulationNodeCount)
+            gammaP.setDimension(speciesReticulationNodeCount);
+
         final int traversalNodeCount = speciesNetwork.getNodeCount() - speciesNetwork.getLeafNodeCount();
-        final int geneNodeCount = geneTree.getNodeCount();
-        for (int i = 0; i < traversalNodeCount; i++) {
-            for (int j = 0; j < geneNodeCount; j++) {
-                embedding.setMatrixValue(i, j, -1);
+        // simulate each gene tree and alignment
+        for (int ig = 0; ig < geneTrees.size(); ig++) {
+            Tree geneTree = geneTrees.get(ig);
+            IntegerParameter embedding = embeddings.get(ig);
+
+            // initialize embedding matrix to -1 (no traversal)
+            final int geneNodeCount = geneTree.getNodeCount();
+            embedding.setDimension(traversalNodeCount * geneNodeCount);
+            embedding.setMinorDimension(geneNodeCount);
+            for (int i = 0; i < traversalNodeCount; i++)
+                for (int j = 0; j < geneNodeCount; j++)
+                    embedding.setMatrixValue(i, j, -1);
+
+            networkNodeGeneLineagesMap.clear();
+            // generate map of tip names to tip nodes
+            final Map<String, NetworkNode> speciesNodeMap = new HashMap<>();
+            for (NetworkNode leafNode: speciesNetwork.getLeafNodes()) {
+                final String speciesName = leafNode.getID();
+                speciesNodeMap.put(speciesName, leafNode);
             }
+            final Map<String, Node> geneNodeMap = new HashMap<>();
+            for (Node leafNode: geneTree.getExternalNodes()) {
+                final String geneName = leafNode.getID();
+                geneNodeMap.put(geneName, leafNode);
+            }
+            // multimap of species network tip node to gene tree tip nodes
+            final TaxonSet taxonSuperSet = taxonSuperSetInput.get();
+            for (Taxon speciesTip: taxonSuperSet.taxonsetInput.get()) {
+                final NetworkNode speciesNode = speciesNodeMap.get(speciesTip.getID());
+                final TaxonSet speciesTaxonSet = (TaxonSet) speciesTip;
+                for (Taxon geneTip: speciesTaxonSet.taxonsetInput.get()) {
+                    final Node geneNode = geneNodeMap.get(geneTip.getID());
+                    networkNodeGeneLineagesMap.put(speciesNode, geneNode);
+                }
+            }
+
+            // reset visited indicator
+            NetworkNode networkRoot = speciesNetwork.getRoot();
+            networkRoot.resetAllVisited();
+
+            // simulate the gene tree
+            nodeIndex = 0;
+            speciesLeafNodeCount = speciesNetwork.getLeafNodeCount();
+            double ploidy = 2.0;
+            if (ploidies != null && ploidies.getDimension() > ig)
+                ploidy = ploidies.getArrayValue(ig);
+            simulateGeneTree(networkRoot, geneTree, embedding, ploidy);
+
+            // simulate alignment on the gene tree
+            if (seqSimulators != null)
+                simulateSequences(ig);
         }
-        // initialize species network tip to gene tree tips map
-        networkNodeGeneLineagesMap.clear();
-        networkNodeGeneLineagesMap.putAll(tipsMap);
-        // reset visited indicator
-        NetworkNode networkRoot = speciesNetwork.getRoot();
-        networkRoot.resetAllVisited();
-
-        // simulate the gene tree
-        nodeIndex = 0;
-        speciesLeafNodeCount = speciesNetwork.getLeafNodeCount();
-        simulateGeneTree(networkRoot);
-
-        // simulate alignment on the gene tree
-        simulateSequences();
-
-        return 0.0;
     }
 
     // recursively simulate lineages coalescent in each population
-    private void simulateGeneTree(NetworkNode snNode) {
+    private void simulateGeneTree(NetworkNode snNode, Tree geneTree, IntegerParameter embedding, double ploidy) {
         if (snNode.isVisited())
             return;
         if (snNode.getLeftChild() != null) {
-            simulateGeneTree(snNode.getLeftChild());
+            simulateGeneTree(snNode.getLeftChild(), geneTree, embedding, ploidy);
         }
         if (snNode.getRightChild() != null) {
-            simulateGeneTree(snNode.getRightChild());
+            simulateGeneTree(snNode.getRightChild(), geneTree, embedding, ploidy);
         }
 
         snNode.setVisited();  // set visited indicator
@@ -149,15 +179,15 @@ public class CoalescentSimulator extends Operator {
 
             final double bottomHeight = snNode.getHeight();
             final int leftBranchNumber = snNode.getLeftBranchNumber();
-            final double leftPopSize = popSizes.getArrayValue(leftBranchNumber);
-            final double leftTopHeight = snNode.getLeftParent().getHeight();
+            final double lPopSize = popSizes.getArrayValue(leftBranchNumber);
+            final double lTopHeight = snNode.getLeftParent().getHeight();
             List<Node> lineagesAtLeftTop =
-                    simulateCoalescentEvents(lineagesAtLeft, bottomHeight, leftTopHeight, ploidy*leftPopSize);
+                    simulateCoalescentEvents(lineagesAtLeft, bottomHeight, lTopHeight, ploidy*lPopSize, geneTree);
             final int rightBranchNumber = snNode.getRightBranchNumber();
-            final double rightPopSize = popSizes.getArrayValue(rightBranchNumber);
-            final double rightTopHeight = snNode.getRightParent().getHeight();
+            final double rPopSize = popSizes.getArrayValue(rightBranchNumber);
+            final double rTopHeight = snNode.getRightParent().getHeight();
             List<Node> lineagesAtRightTop =
-                    simulateCoalescentEvents(lineagesAtRight, bottomHeight, rightTopHeight, ploidy*rightPopSize);
+                    simulateCoalescentEvents(lineagesAtRight, bottomHeight, rTopHeight, ploidy*rPopSize, geneTree);
 
             networkNodeGeneLineagesMap.putAll(leftParent, lineagesAtLeftTop);
             networkNodeGeneLineagesMap.putAll(rightParent, lineagesAtRightTop);
@@ -181,7 +211,8 @@ public class CoalescentSimulator extends Operator {
             else
                 topHeight = snNode.getRightParent().getHeight();
 
-            List<Node> lineagesAtTop = simulateCoalescentEvents(lineagesAtBottom, bottomHeight, topHeight, ploidy*popSize);
+            List<Node> lineagesAtTop =
+                    simulateCoalescentEvents(lineagesAtBottom, bottomHeight, topHeight, ploidy*popSize, geneTree);
             if (leftParent != null) {
                 networkNodeGeneLineagesMap.putAll(leftParent, lineagesAtTop);
                 // update embedding
@@ -200,7 +231,8 @@ public class CoalescentSimulator extends Operator {
         }
     }
 
-    private List<Node> simulateCoalescentEvents(Collection<Node> lineages, double bottomHeight, double topHeight, double pNu) {
+    private List<Node> simulateCoalescentEvents(Collection<Node> lineages, double bottomHeight,
+                                                double topHeight, double pNu, Tree geneTree) {
         // start from the lineages at the bottom
         List<Node> currentLineages = new ArrayList<>(lineages);
         double currentHeight = bottomHeight;
@@ -235,9 +267,8 @@ public class CoalescentSimulator extends Operator {
         return currentLineages;
     }
 
-    private void simulateSequences() {
-        SequenceSimulator seqSimulator = seqSimulatorInput.get();
-        if (seqSimulator == null) return;
+    private void simulateSequences(int locus) {
+        SequenceSimulator seqSimulator = seqSimulators.get(locus);
 
         Alignment alignment = seqSimulator.simulate();
 
