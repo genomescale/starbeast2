49d4d967bd9d12eb0e539a8401359ae4117ec753
Tim Vaughan <tgvaughan@gmail.com>
2017-05-01 16:25:03 +1200

    TreeTopologyDistLogger is now a real logger.

--- src/starbeast2/utils/TreeTopologyDistLogger.java
+++ src/starbeast2/utils/TreeTopologyDistLogger.java
@@ -3,10 +3,13 @@ package starbeast2.utils;
 import beast.core.BEASTObject;
 import beast.core.Input;
 import beast.core.Loggable;
+import beast.core.Logger;
 import beast.evolution.tree.Node;
 import beast.evolution.tree.Tree;
 import beast.evolution.tree.TreeTraceAnalysis;
+import beast.util.FrequencySet;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,16 +18,16 @@ import java.util.List;
 /**
  * Created by Tim Vaughan <tgvaughan@gmail.com> on 28/04/17.
  */
-public class TreeTopologyDistLogger extends BEASTObject implements Loggable {
+public class TreeTopologyDistLogger extends Logger {
 
     public Input<Tree> treeInput = new Input<>(
             "tree",
             "Tree whose topology to consider",
             Input.Validate.REQUIRED);
 
-    public Input<Double> burninInput = new Input<>(
-            "burnInFrac",
-           "Burn-in fraction omitted in analysis.",
+    public Input<Integer> burninSamplesInput = new Input<>(
+            "burninSamples",
+           "Number of burn-in samples omitted from analysis.",
             Input.Validate.REQUIRED);
 
     public Input<Boolean> isSpeciesTreeInput = new Input<>(
@@ -33,60 +36,89 @@ public class TreeTopologyDistLogger extends BEASTObject implements Loggable {
 
 
     Tree tree;
-    List<Tree> treeList;
-    boolean isSpeciesTree;
+    ModifiedTreeTraceAnalysis analysis;
+    int burninSamples, nTreesTotal, nBurninTrees;
+
+    public TreeTopologyDistLogger() {
+        loggersInput.setRule(Input.Validate.OPTIONAL);
+        loggersInput.setValue(new DummyLoggable(), this);
+    }
 
     @Override
     public void initAndValidate() {
+        super.initAndValidate();
+
         tree = treeInput.get();
-        treeList = new ArrayList<>();
-        isSpeciesTree = isSpeciesTreeInput.get();
-    }
 
-    void removeFakeNodes(Node node) {
-        if (node.isFake()) {
-            Node daChild = node.getDirectAncestorChild();
-            node.setNr(daChild.getNr());
-            node.setID(daChild.getID());
-            node.removeChild(daChild);
-        }
+        burninSamples = burninSamplesInput.get();
 
-        for (Node child : node.getChildren())
-            removeFakeNodes(child);
+        analysis = new ModifiedTreeTraceAnalysis(isSpeciesTreeInput.get());
+
     }
 
     @Override
-    public void init(PrintStream out) {
+    public void init() throws IOException {
+        super.init();
+
+        nTreesTotal = 0;
+        nBurninTrees = 0;
     }
 
-    @Override
-    public void log(int sample, PrintStream out) {
-        treeList.add(tree.copy());
-    }
 
     @Override
-    public void close(PrintStream out) {
-        TreeTraceAnalysis analysis;
+    public void log(int sample) {
+        nTreesTotal += 1;
 
-        if (isSpeciesTree)
-            analysis = new ModifiedTreeTraceAnalysis(treeList, burninInput.get());
+        if (sample> burninSamples)
+            analysis.addTree(tree);
         else
-            analysis = new TreeTraceAnalysis(treeList, burninInput.get());
-
-        analysis.analyze(1.0);
-        analysis.report(out);
+            nBurninTrees += 1;
     }
 
+    @Override
+    public void close() {
+        analysis.report(getM_out(), nTreesTotal, nBurninTrees);
+    }
+
+    /**
+     * TreeTraceAnalysis class with some modifications to support sampled
+     * ancestors and incremental sample addition.
+     */
     class ModifiedTreeTraceAnalysis extends TreeTraceAnalysis {
 
-        public ModifiedTreeTraceAnalysis(List<Tree> posteriorTreeList, double burninFraction) {
-            super(posteriorTreeList, burninFraction);
+        boolean isSpeciesTree;
+
+        public ModifiedTreeTraceAnalysis(boolean isSpeciesTree) {
+            super(new ArrayList<>(), 0.1);
+
+            this.isSpeciesTree = isSpeciesTree;
+            topologiesFrequencySet = new FrequencySet<>();
+            topologiesFrequencySet.setCredSetProbability(1.0);
         }
 
         /**
-         * get tree topology in Newick that is sorted by taxon labels.
-         * @param node
-         * @return
+         * Add topology corresponding to given tree to topolgoy frequency set.
+         *
+         * @param tree tree whose topology to add
+         */
+        public void addTree(Tree tree) {
+            String topology = uniqueNewick(tree.getRoot());
+            topologiesFrequencySet.add(topology, 1);
+        }
+
+        public void report(PrintStream oStream, int totalTrees, int burnin) {
+            credibleSet = topologiesFrequencySet.getCredibleSet();
+            this.totalTrees = totalTrees;
+            this.burnin = burnin;
+
+            super.report(oStream);
+        }
+
+        /**
+         * Get tree topology in Newick that is sorted by taxon labels.
+         *
+         * @param node root of tree
+         * @return newick string
          */
         String getSortedNewickWithSAs(Node node) {
             if (node.isLeaf()) {
@@ -121,5 +153,22 @@ public class TreeTopologyDistLogger extends BEASTObject implements Loggable {
         public String uniqueNewick(Node node) {
             return getSortedNewickWithSAs(node);
         }
+    }
+
+    class DummyLoggable extends BEASTObject implements Loggable {
+
+        public DummyLoggable() { }
+
+        @Override
+        public void init(PrintStream out) { }
+
+        @Override
+        public void log(int sample, PrintStream out) { }
+
+        @Override
+        public void close(PrintStream out) { }
+
+        @Override
+        public void initAndValidate() { }
     }
 }
