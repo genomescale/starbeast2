4dd4af2309507fb15f68e6d46789d879afe43bcc
genomescale <huw@huw.id.au>
2020-12-21 12:17:00 -0600

    Alternative parameterization of InverseGamma (shape, mean)

---
+++ src/beast/math/distributions/AltInverseGamma.java
@@ -0,0 +1,32 @@
+package beast.math.distributions;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.RealParameter;
+
+@Description("Inverse Gamma distribution, used as prior. Parameterized by its mean instead of scale.")
+
+public class AltInverseGamma extends InverseGamma {
+    final public Input<RealParameter> meanInput = new Input<>("mean", "mean of the distribution, defaults to 1");
+
+    void refresh() {
+        double alpha;
+        double mean;
+
+        if (alphaInput.get() == null) {
+            alpha = 2;
+        } else {
+            alpha = alphaInput.get().getValue();
+        }
+
+        if (meanInput.get() == null) {
+            mean = 1;
+        } else {
+            mean = meanInput.get().getValue();
+        }
+
+        final double beta = mean * (alpha - 1);
+
+        dist.setAlphaBeta(alpha, beta);
+    }
+}
